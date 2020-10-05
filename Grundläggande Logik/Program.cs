64ad4b91e8885a15237596c7f5a17ce6e9b09928
Lukas <lukas.tuna03@gmail.com>
2020-10-05 09:36:07 +0200

    Sista commit

--- Grundläggande Logik/Program.cs
+++ Grundläggande Logik/Program.cs
@@ -6,7 +6,40 @@ namespace Grundläggande_Logik
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            string x = "";
+
+            while (!(x == "5"))
+            {
+                System.Console.WriteLine("Skriv en siffra mellan 0-10: ");
+                x = Console.ReadLine();
+
+                if (x == "1" || x == "2" || x == "8" || x == "9")
+                {
+                    Console.WriteLine("miss");
+                }
+
+                if (x == "3" || x == "4" || x == "6" || x == "7")
+                {
+                    Console.WriteLine("near miss");
+
+
+                }
+                if (x == "0" || x == "10")
+                {
+                    Console.WriteLine("Jag sa ett nummer mellan 0-10 ");
+                }
+
+
+
+
+
+
+            }
+
+            System.Console.WriteLine("rätt");
+
+            Console.ReadLine();
         }
     }
 }
