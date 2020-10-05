7b6cce75de8e74a4999d9928f0c6558eae762922
Lukas <lukas.tuna03@gmail.com>
2020-10-05 08:41:17 +0200

    Initial commit

---
+++ Grundläggande Logik/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Grundläggande_Logik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
